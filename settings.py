4f7e6677fd32d95d4bcb73022595428c4da9d2d7
Deadflagx <68439718+Deadflagx@users.noreply.github.com>
2021-04-08 13:34:13 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,4 +2,4 @@ group_id =			#Vk Group Id
 topic_id = 			#Vk Topic Id for list of followings
 token = 			#Vk Token(User)
 inst_login = 		        #Instagram Login
-inst_password = 	      #Instagram Password
+inst_password = 	        #Instagram Password
